b87fd5def3f7835cf993c644a7e6787babc16eda
Yehuda Ringler <yrappdev@gmail.com>
2021-04-25 00:08:41 -0400

    add more just_audio, more design to persist position

--- just_audio_handlers/lib/src/handler_just_audio.dart
+++ just_audio_handlers/lib/src/handler_just_audio.dart
@@ -1,12 +1,37 @@
+import 'dart:async';
 import 'package:audio_service/audio_service.dart';
+import 'package:just_audio/just_audio.dart';
 
 /// Uses just_audio to handle playback.
 class AudioHandlerJustAudio extends BaseAudioHandler with SeekHandler {
+  final AudioPlayer player;
+
+  AudioHandlerJustAudio({required this.player}) {
+    player.playbackEventStream
+        .listen((event) => playbackState.add(justAudioToAudioService(event)));
+  }
+
   @override
-  Future<void> prepareFromUri(Uri uri, [Map<String, dynamic>? extras]) async {}
+  Future<void> prepareFromUri(Uri uri, [Map<String, dynamic>? extras]) async {
+    final currentState = playbackState.valueWrapper?.value ?? PlaybackState();
+    playbackState.add(currentState.copyWith(playing: false));
+
+    await player.setUrl(uri.toString());
+  }
+
+  @override
+  Future<void> prepareFromMediaId(String mediaId,
+      [Map<String, dynamic>? extras]) async {}
 
   @override
   Future<void> playFromUri(Uri uri, [Map<String, dynamic>? extras]) async {}
+
+  @override
+  Future<void> play() async {}
+
+  @override
+  Future<void> playFromMediaId(String mediaId,
+      [Map<String, dynamic>? extras]) async {}
 
   @override
   Future<void> pause() async {}
@@ -16,4 +41,34 @@ class AudioHandlerJustAudio extends BaseAudioHandler with SeekHandler {
 
   @override
   Future<void> setSpeed(double speed) async {}
+
+  PlaybackState justAudioToAudioService(PlaybackEvent event) {
+    final currentState = playbackState.valueWrapper?.value ?? PlaybackState();
+    final playing = player.playing;
+
+    return currentState.copyWith(
+      controls: [
+        if (playing) MediaControl.pause else MediaControl.play,
+        MediaControl.stop,
+      ],
+      systemActions: const {
+        MediaAction.seek,
+        MediaAction.seekForward,
+        MediaAction.seekBackward,
+      },
+      androidCompactActionIndices: const [0, 1, 3],
+      processingState: const {
+        ProcessingState.idle: AudioProcessingState.idle,
+        ProcessingState.loading: AudioProcessingState.loading,
+        ProcessingState.buffering: AudioProcessingState.buffering,
+        ProcessingState.ready: AudioProcessingState.ready,
+        ProcessingState.completed: AudioProcessingState.completed,
+      }[player.processingState]!,
+      playing: playing,
+      updatePosition: player.position,
+      bufferedPosition: player.bufferedPosition,
+      speed: player.speed,
+      queueIndex: event.currentIndex,
+    );
+  }
 }
--- just_audio_handlers/lib/src/handler_persist_position.dart
+++ just_audio_handlers/lib/src/handler_persist_position.dart
@@ -1,11 +1,64 @@
 import 'package:audio_service/audio_service.dart';
 
+/// Persists the current location. Updates it whenever there's reason to think that
+/// it has changed. For example, before calling playFromUri, stop, etc.
 class AudioHandlerPersistPosition extends CompositeAudioHandler {
   final PositionSaver positionRepository;
 
   AudioHandlerPersistPosition(AudioHandler inner,
       {required this.positionRepository})
       : super(inner);
+
+  @override
+  Future<void> play() async {}
+
+  @override
+  Future<void> playFromMediaId(String mediaId,
+      [Map<String, dynamic>? extras]) async {}
+
+  @override
+  Future<void> playFromSearch(String query,
+      [Map<String, dynamic>? extras]) async {}
+
+  @override
+  Future<void> playFromUri(Uri uri, [Map<String, dynamic>? extras]) async {}
+
+  @override
+  Future<void> playMediaItem(MediaItem mediaItem) async {}
+
+  @override
+  Future<void> pause() async {}
+
+  @override
+  Future<void> stop() async {
+    await super.stop();
+  }
+
+  @override
+  Future<void> skipToNext() async {}
+
+  @override
+  Future<void> skipToQueueItem(int index) async {}
+
+  @override
+  Future<void> seek(Duration position) async {
+    await super.seek(position);
+
+    final value = mediaItem.valueWrapper?.value;
+    if (value != null) {
+      positionRepository.set(value.id, position);
+    }
+  }
+
+  @override
+  Future<void> onTaskRemoved() async {}
+
+  @override
+  Future<void> onNotificationDeleted() async {
+    await stop();
+  }
+
+  Future<void> _updateSavedPosition() async {}
 }
 
 abstract class PositionSaver {
